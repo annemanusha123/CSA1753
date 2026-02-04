9891413c5636e075b80c650745656a2308e9bb80
annemanusha123 <192472083.simats@saveetha.com>
2026-02-04 11:01:09 +0530

    Add files via upload

---
+++ 8- PUZZLE PROGRAM.py
@@ -0,0 +1,7 @@
+Python 3.14.0b4 (tags/v3.14.0b4:7ec1fab, Jul  8 2025, 12:39:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/kittu/AppData/Local/Programs/Python/Python314/8- PUZZLE PROGRAM(1).py
+((1, 2, 3), (4, 5, 6), (7, 8, 0))
+((1, 2, 3), (4, 5, 6), (7, 0, 8))
+((1, 2, 3), (4, 0, 6), (7, 5, 8))
